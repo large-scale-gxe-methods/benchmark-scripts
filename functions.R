cc5069e7c05290c29a4602542d1d7ce533977aea
akmanning <amanning@broadinstitute.org>
2020-01-03 09:33:46 -0500

    refined visualization and summaries

--- functions.R
+++ functions.R
@@ -23,11 +23,11 @@ convb <- function(x){
 
 make_cpu_plot <- function(cpu_table) {
   cpu_table <- data.frame(cbind(time=seq(1,nrow(cpu_table)),
-                     usr=as.numeric(cpu_table[,"cpu-usr"]),
-                     sys=as.numeric(cpu_table[,"cpu-sys"]),
-                     idl=as.numeric(cpu_table[,"cpu-idl"]),
-                     wai=as.numeric(cpu_table[,"cpu-wai"]),
-                     stl=as.numeric(cpu_table[,"cpu-stl"])))
+                     usr=as.numeric(ifelse(cpu_table[,"cpu-usr"]=="-",-1,cpu_table[,"cpu-usr"])),
+                     sys=as.numeric(ifelse(cpu_table[,"cpu-sys"]=="-",-1,cpu_table[,"cpu-sys"])), #cpu_table[,"cpu-sys"]),
+                     idl=as.numeric(ifelse(cpu_table[,"cpu-idl"]=="-",-1,cpu_table[,"cpu-idl"])), #cpu_table[,"cpu-idl"]),
+                     wai=as.numeric(ifelse(cpu_table[,"cpu-wai"]=="-",-1,cpu_table[,"cpu-wai"])), #cpu_table[,"cpu-wai"]),
+                     stl=as.numeric(ifelse(cpu_table[,"cpu-stl"]=="-",-1,cpu_table[,"cpu-stl"])))) #cpu_table[,"cpu-stl"])))
   
   ggplot(data=cpu_table,aes(x=time)) + 
     geom_line(aes(y=usr,color="usr")) +
@@ -36,7 +36,7 @@ make_cpu_plot <- function(cpu_table) {
     geom_line(aes(y=wai,color="wai")) +
     geom_line(aes(y=stl,color="stl")) +
     scale_colour_manual("", values = c("usr"="blue", "sys"="purple","idl"="orange","wai"="pink","stl"="red")) +
-    scale_y_continuous("Percent CPU", limits = c(0,100))
+    scale_y_continuous("% CPU", limits = c(0,100))
 }
 
 make_dsk_plot <- function(dsk_table) {
@@ -47,7 +47,7 @@ make_dsk_plot <- function(dsk_table) {
   ggplot(data=dsk_table,aes(x=time)) + 
     geom_line(aes(y=read,color="read")) +
     geom_line(aes(y=write,color="write")) + scale_colour_manual("", values = c("read"="blue", "write"="purple")) +
-    scale_y_continuous("Read/Write in Bytes",trans="log10")
+    scale_y_continuous("Read/Write (B)",trans="log10")
   
 }
 
@@ -64,7 +64,7 @@ make_mem_plot <- function(mem_table) { # used  free  buff  cach
     geom_line(aes(y=buffered,color="buffered")) +
     geom_line(aes(y=cached,color="cached")) +
     scale_colour_manual("", values = c("used"="blue", "free"="purple","buffered"="orange","cached"="red")) +
-    scale_y_continuous("Memory in Bytes",trans="log10")
+    scale_y_continuous("Memory (B)",trans="log10")
 }
   
 parameters = c("bgen file",
@@ -81,26 +81,52 @@ plot_log_file_data <- function(parameters_table,log_table,plot_description) {
   library(grid)
   library(gridExtra)
   library(gdata)
-  
-  main_title <- textGrob(plot_description,gp=gpar(fontsize=8))
+ # print("main title")
+  main_title <- textGrob(plot_description,gp=gpar(fontsize=6))
+  #cat(plot_description)
+ # grid.newpage()
+#  grid.text(plot_description,gp=gpar(fontsize=8),hjust=0)
+  #print("cpu_plot")
   cpu_plot <- make_cpu_plot(log_table[,1:5])
+#  print("dsk_plot")
+  
   dsk_plot <- make_dsk_plot(log_table[,6:7])
+ # print("mem_plot")
+  
   mem_plot <- make_mem_plot(log_table[,8:11])
 
   grid.arrange(main_title,
-                tableGrob(parameters_table,theme=ttheme_default(base_size = 6)),
+#  grid.newpage()
+                tableGrob(parameters_table[1:4,],rows=rownames(parameters_table)[1:4],theme=ttheme_default(base_size = 6)),
+                tableGrob(parameters_table[5:8,],rows=rownames(parameters_table)[5:8],theme=ttheme_default(base_size = 6)),
                 cpu_plot,
                 dsk_plot,
-                mem_plot,layout_matrix=matrix(c(1,2,3,3,4,4,5,5),ncol=2,nrow=4,byrow = TRUE))
+                mem_plot,layout_matrix=matrix(c(1,1,1,1,1,1,1,1,2,4,5,6,3,4,5,6),ncol=4,nrow=4,byrow = FALSE))
   
 }
 
-do_plots <- function(path_to_workflow,cpus,disk,memory,maf,stream_snps,run_tests_runtime=NA,plot_description) {
-  bgen_files_size <- get_bgen_files_size(path_to_workflow)
+do_plots <- function(path_to_workflow,cpus,disk,memory,maf,stream_snps,run_tests_runtime=NA,plot_description,plot_data=NA,chr=NA) {
+  if(!is.na(plot_data)) {
+    bgen_files_size = plot_data[["bgen_files_size"]]
+    log_file_data = plot_data[["log_file_data"]]
+    terra_time_data = plot_data[["terra_time_data"]]
+  } else {
+    
+    bgen_files_size <- get_bgen_files_size(path_to_workflow)
+    log_file_data <- get_log_files(path_to_workflow)
+    terra_time_data <- get_terra_time_data(path_to_workflow)
+  }
+  #index.time <- grep(pattern="Total Wall Time",terra_time_data)
 
-  log_file_data <- get_log_files(path_to_workflow)
-
-  for(i in 1:nrow(bgen_files_size)) {
+  if(!is.na(chr)) {
+    chr.index <- grep(pattern=chr,bgen_files_size[,1],fixed=TRUE)
+  } else {
+    chr.index <- seq(1,nrow(bgen_files_size))
+  }
+  
+  for(i in chr.index) {
+ #   print(i)
+#    print(bgen_files_size[i,])
    parameter_values = c("bgen file"=basename(bgen_files_size[i,1]),
                        "bgen size"=bgen_files_size[i,2],
                        "workflow CPUs"=cpus,
@@ -109,11 +135,12 @@ do_plots <- function(path_to_workflow,cpus,disk,memory,maf,stream_snps,run_tests
                        "workflow maf"=maf,
                        "workflow stream_snps"=stream_snps,
                        "run_tests task duration"=run_tests_runtime)
-  
+ # print(parameter_values)
   parameters_table <- matrix(parameter_values,ncol=1,dimnames = list(parameters,"values"))
-  
+#  print(parameters_table)
   plot_log_file_data(parameters_table=parameters_table,
                      log_table=log_file_data[[i]],
-                     plot_description=plot_description)
+                     plot_description=paste(plot_description,paste(terra_time_data[[i]],collapse="\n"),sep="\n\n"))
   }
+  return(list(bgen_files_size=bgen_files_size,log_file_data=log_file_data,terra_time_data=terra_time_data))
 }
